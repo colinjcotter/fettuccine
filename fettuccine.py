565e7a5969775a83c65ab6a39f377971cd706c8b
Colin Cotter <colin.cotter@imperial.ac.uk>
2022-07-27 18:05:20 +0100

    simple solver works

--- fettuccine.py
+++ fettuccine.py
@@ -7,7 +7,7 @@ x = SpatialCoordinate(mesh)
 
 deg = 1
 NewX = Function(VectorFunctionSpace(mesh, "CG", 1, dim=3))
-NewX.interpolate(as_vector([x[0],0,0]))
+NewX.interpolate(as_vector([pi*x[0],0,0]))
 
 mesh = Mesh(Function(NewX))
 x = SpatialCoordinate(mesh)
@@ -16,40 +16,59 @@ V = VectorFunctionSpace(mesh, "CG", deg)
 Q = VectorFunctionSpace(mesh, "DG", deg-1)
 A = FunctionSpace(mesh, "DG", deg-1)
 
-# q, t, b, p, lambda, mu, alpha, beta
+# q, t, b, p, lambda, mu, alpha, gamma
 W = V * V * V * Q * Q * Q * A * A
 
 w = Function(W)
 dw = TestFunction(W)
 
-#boundary conditions
-qstar0 = Constant(as_vector([0,0,0]))
-qstar1 = Constant(as_vector([1.0,0,0]))
-tstar0 = Constant(as_vector([1.0,0.,0]))
-tstar1 = Constant(as_vector([1.0,0.,0]))
-bstar0 = Constant(as_vector([0,1.0,0]))
-bstar1 = Constant(as_vector([0,1.0,0]))
 
 #initial guesses
-# q, t, b, p, lambda, mu, alpha, beta
+# q, t, b, p, lambda, mu, alpha, gamma
 q, t, b, p, ll, mu, alpha, beta = w.split()
+
+s, _, _ = SpatialCoordinate(mesh)
+
+def q_para(s):
+    return as_vector([-sin(s), cos(s), 0])
+
+def t_para(s):
+    return as_vector([-cos(s), -sin(s), 0])
+
+
+#boundary conditions
+qstar0 = q_para(Constant(0))
+qstar1 = q_para(Constant(1))
+tstar0 = t_para(Constant(0))
+tstar1 = t_para(Constant(1))
+bstar0 = Constant(as_vector([0,0.0,1]))
+bstar1 = Constant(as_vector([0,0.0,1]))
+
+q.interpolate(q_para(s))
+t.interpolate(t_para(s))
+b.interpolate(as_vector([0, 0, 1]))
+alpha.assign(1.0)
+beta.assign(0.)
 
 #splitting for equations
 q, t, b, p, ll, mu, alpha, beta = split(w)
 
-kappasq = alpha**2
-tausq = alpha**2 + beta**2
-F = ((kappasq + tausq)**2/kappasq)*dx
+#F = alpha**2*(2+ gamma**2)
+F = (2*alpha**2 + beta**2) # /alpha**2
 
-inner(p, q.dx(0))
-
-S = F + (
-    inner(ll, t.dx(0) - alpha*cross(b, t))
-    + inner(mu, b.dx(0) - beta*cross(t, b))
-    + inner(p, q.dx(0) - t)
+S = (F +
+     inner(ll, t.dx(0) - alpha*cross(b, t))
+     + inner(mu, b.dx(0) - beta*cross(t, b))
+     + inner(p, q.dx(0) - t)
 )*dx
 
 eqn = derivative(S, w, dw)
+
+S1 = S + inner(p, p)*dx + inner(mu, mu)*dx + inner(ll, ll)*dx
+S1 += inner(q, q)*dx + inner(t, t)*dx + inner(b, b)*dx
+eqn1 = derivative(S1, w)
+Jp = derivative(eqn1, w)
+
 
 bcs = [DirichletBC(W.sub(0), qstar0, 1),
        DirichletBC(W.sub(0), qstar1, 2),
@@ -57,15 +76,14 @@ bcs = [DirichletBC(W.sub(0), qstar0, 1),
        DirichletBC(W.sub(1), tstar1, 2),
        DirichletBC(W.sub(2), bstar0, 1),
        DirichletBC(W.sub(2), bstar1, 2)]
-       
 
-solver_parameters = {'ksp_type':'preonly',
+solver_parameters = {'ksp_type':'gmres',
+                     'ksp_monitor': None,
                      'mat_type':'aij',
                      'pc_type':'lu',
+                     "pc_factor_mat_ordering_type": "rcm",
+                     "pc_factor_shift_type": "nonzero",
                      'snes_monitor':None}
 
-q, ll, mu, p, b, t, alpha, gamma = TrialFunctions(W)
-dq, dll, dmu, dp, db, dt, dalpha, dgamma = TestFunctions(W)
-
-solve(eqn == 0, w, bcs = bcs,
+solve(eqn == 0, w, bcs = bcs, Jp=Jp,
       solver_parameters = solver_parameters)
